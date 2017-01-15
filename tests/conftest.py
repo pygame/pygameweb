1c7493fac971db0fd0d80ce09d93cf9f5f3707ab
Rene Dudfield <renesd@gmail.com>
2017-01-15 14:43:54 +1100

    Starting to add testing fixtures.

---
+++ tests/conftest.py
@@ -0,0 +1,9 @@
+""" Fixtures for testing.
+
+http://stackoverflow.com/questions/34466027/in-py-test-what-is-the-use-of-conftest-py-files
+
+To make testing databases easier we need a few fixtures for rolling back changes and such.
+
+"""
+
+
