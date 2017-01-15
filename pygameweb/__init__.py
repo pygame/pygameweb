fe6bf7465f279a00154f3c0b71ba8586beabec66
Rene Dudfield <renesd@gmail.com>
2017-01-15 16:43:15 +1100

    Add flask factory.

--- pygameweb/__init__.py
+++ pygameweb/__init__.py
@@ -1,1 +1,14 @@
 __version__ = '0.0.0'
+
+
+def create_app(object_name='pygameweb.config.Config'):
+    """returns a flask app.
+
+    http://flask.pocoo.org/docs/patterns/appfactories/
+    """
+
+    from flask import Flask
+    app = Flask(__name__)
+    app.config.from_object(object_name)
+
+    return app
