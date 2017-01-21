c28ff14a2cad5e304a87bc7664ba048be548aa5a
Rene Dudfield <renesd@gmail.com>
2017-01-21 21:05:26 +1100

    Started adding wiki routes.

--- pygameweb/wiki/models.py
+++ pygameweb/wiki/models.py
@@ -2,7 +2,24 @@
 """
 
 from sqlalchemy import Column, DateTime, Integer, String, Text
+import feedparser
 from pygameweb.models import Base
+
+
+from pygameweb.wiki.wiki import render
+
+
+def sanitize_html(html):
+    """ santise_html(html) returns some sanitized html.
+          It can be used to try and avoid basic html insertion attacks.
+
+        >>> sanitize_html("<p>hello</p>")
+        '<p>hello</p>'
+        >>> sanitize_html("<script>alert('what')</script>")
+        ''
+    """
+    return feedparser._sanitizeHTML(html, "utf-8", "text/html")
+
 
 class Wiki(Base):
     __tablename__ = 'wiki'
@@ -20,3 +37,15 @@ class Wiki(Base):
     title = Column(String(255))
     parent = Column(String(255))
     keywords = Column(String(255))
+
+    @property
+    def content_rendered(self):
+        """The wiki content is rendered for display.
+        """
+        return sanitize_html(render(self.content))
+
+    @property
+    def content_sanitized(self):
+        """The wiki content which has any html sanitized.
+        """
+        return sanitize_html(self.content)
--- pygameweb/wiki/views.py
+++ pygameweb/wiki/views.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, render_template, abort
-# The sqlalchemy session is per request.
 # http://flask-sqlalchemy-session.readthedocs.org/en/v1.1/
 from flask_sqlalchemy_session import current_session
-# from flask_admin import expose, AdminIndexView
+
+from pygameweb.wiki.models import Wiki
+
+
+
 
 wiki_blueprint = Blueprint('wiki',
                            __name__,
@@ -10,10 +13,43 @@ wiki_blueprint = Blueprint('wiki',
 
 
 @wiki_blueprint.route('/wiki/', methods=['GET'])
-def wiki():
+@wiki_blueprint.route('/wiki/<link>', methods=['GET'])
+def index(link='index'):
     """
     """
-    return render_template('wiki.html')
+    result = (current_session
+              .query(Wiki)
+              .filter(Wiki.link == link)
+              .filter(Wiki.latest == 1)
+              .first())
+
+    if not result:
+        abort(404)
+
+    return render_template('wiki.html', wiki=result)
+
+
+@wiki_blueprint.route('/wiki/<link>/edit', methods=['GET'])
+def edit(link):
+    """
+    """
+    result = (current_session
+              .query(Wiki)
+              .filter(Wiki.link == link)
+              .filter(Wiki.latest == 1)
+              .first())
+
+    if not result:
+        abort(404)
+
+    return render_template('wiki_edit.html', wiki=result)
+
+
+# @wiki_blueprint.route('/wiki/<link>/edit', methods=['POST'])
+# def save(link):
+#     """
+#     """
+
 
 
 def add_wiki_blueprint(app):
