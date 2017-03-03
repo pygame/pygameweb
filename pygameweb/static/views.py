9898770f5c86b838c1db4ecca8de0131715f3d92
Rene Dudfield <renesd@gmail.com>
2017-03-03 18:55:52 +0100

    serve favicon.ico

--- pygameweb/static/views.py
+++ pygameweb/static/views.py
@@ -9,7 +9,7 @@ static_blueprint = Blueprint('static',
                              template_folder='../templates/')
 
 
-files = ['lofi.html', 'server.json', 'hifi.html']
+files = ['lofi.html', 'server.json', 'hifi.html', 'favicon.ico']
 folders = ['content', 'contests', 'css', 'ctypes', 'docs', 'docs-old', 'ftp',
            'galleries', 'gamelets', 'games', 'html5media', 'images',
            'interview', 'iscroll', 'js', 'ludumcontest1', 'ludumcontest2',
