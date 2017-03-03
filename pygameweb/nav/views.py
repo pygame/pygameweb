498328297a70d5644d548dced083914fe72c2af0
Rene Dudfield <renesd@gmail.com>
2017-03-03 18:31:14 +0100

    Put the snakey logo in the nav bar.

--- pygameweb/nav/views.py
+++ pygameweb/nav/views.py
@@ -13,6 +13,73 @@ from flask_nav import Nav
 from flask_nav.elements import Navbar, Subgroup, Link, View
 
 
+
+# hackity hack the navbar...
+from hashlib import sha1
+from dominate import tags
+from visitor import Visitor
+
+import flask_bootstrap.nav
+BootstrapRendererOld = flask_bootstrap.nav.BootstrapRenderer
+
+class BootstrapRendererNew(BootstrapRendererOld):
+    """ We have to hack this to add an image.
+
+    Should probably just do the navigation ourselves.
+    """
+
+    def visit_Navbar(self, node):
+        # create a navbar id that is somewhat fixed, but do not leak any
+        # information about memory contents to the outside
+        node_id = self.id or sha1(str(id(node)).encode()).hexdigest()
+
+        root = tags.nav() if self.html5 else tags.div(role='navigation')
+        root['class'] = 'navbar navbar-default'
+
+        cont = root.add(tags.div(_class='container-fluid'))
+
+        # collapse button
+        header = cont.add(tags.div(_class='navbar-header'))
+        btn = header.add(tags.button())
+        btn['type'] = 'button'
+        btn['class'] = 'navbar-toggle collapsed'
+        btn['data-toggle'] = 'collapse'
+        btn['data-target'] = '#' + node_id
+        btn['aria-expanded'] = 'false'
+        btn['aria-controls'] = 'navbar'
+
+        btn.add(tags.span('Toggle navigation', _class='sr-only'))
+        btn.add(tags.span(_class='icon-bar'))
+        btn.add(tags.span(_class='icon-bar'))
+        btn.add(tags.span(_class='icon-bar'))
+
+        # title may also have a 'get_url()' method, in which case we render
+        # a brand-link
+        if node.title is not None:
+            if hasattr(node.title, 'get_url'):
+                # a_tag = tags.a(node.title.text, _class='navbar-brand',
+                #                   href=node.title.get_url())
+                a_tag = tags.a(_class='navbar-left', title=node.title.text, href=node.title.get_url())
+                a_tag.add(tags.img(src='images/logo_lofi.png', style='max-width:100px;margin-top: 10px;'))
+                header.add(a_tag)
+            else:
+                header.add(tags.span(node.title, _class='navbar-brand'))
+
+        bar = cont.add(tags.div(
+            _class='navbar-collapse collapse',
+            id=node_id,
+        ))
+        bar_list = bar.add(tags.ul(_class='nav navbar-nav'))
+
+        for item in node.items:
+            bar_list.add(self.visit(item))
+
+        return root
+
+
+flask_bootstrap.nav.BootstrapRenderer = BootstrapRendererNew
+
+
 def make_nav(session):
     """ Creates the navigation using flask-nav.
     """
