99c19a02e227ce13a96f608483f2da034928e1b6
Rene Dudfield <renesd@gmail.com>
2017-03-16 09:14:53 +0100

    If there's no email for the comment, don't use a picture.

--- pygameweb/user/models.py
+++ pygameweb/user/models.py
@@ -8,7 +8,7 @@
 
 """
 
-from sqlalchemy import Column, Integer, String, Table, ForeignKey, DateTime, Boolean
+from sqlalchemy import Column, Integer, String, Table, ForeignKey, DateTime, Boolean, func
 from sqlalchemy.orm import relationship, backref
 
 from flask_security import (Security, SQLAlchemyUserDatastore,
@@ -28,29 +28,53 @@ class User(Base, UserMixin):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(80), unique=True)
+    """ The user name.
+    """
     email = Column(String(80), unique=True)
-
+    """ Their email address.
+    """
     password = Column(String(255))
     """ new password field.
     """
-
     title = Column(String(80))
+    """ The full name, or title they want to give themselves.
+    """
     disabled = Column(Integer, default=0)
+    """ Is 1 if the account was disabled for spam or abuse.
+    """
     super = Column(Integer, default=0)
-
+    """ Is 1 if they are a particularly super user.
+    """
     roles = relationship('Group',
                          secondary=users_groups,
                          backref=backref('users', lazy='dynamic'))
-
     active = Column(Boolean())
     # https://flask-security-fork.readthedocs.io/en/latest/models.html#additional-functionality
     confirmed_at = Column(DateTime)
-
     last_login_at = Column(DateTime)
     current_login_at = Column(DateTime)
     last_login_ip = Column(String(80))
     current_login_ip = Column(String(80))
     login_count = Column(Integer)
+    # registered_at = Column(DateTime(timezone=True), server_default=func.now())
+    # """ When they registered.
+    # """
+    # registered_ip = Column(String(80))
+    # """ Ip when they registered.
+    # """
+    # twitter_user = Column(String(255))
+    # """ Twitter username.
+    # """
+    # github_user = Column(String(255))
+    # """ Github username.
+    # """
+    # bitbucket_user = Column(String(255))
+    # """ Bitbucket username.
+    # """
+    # blog_url = Column(String(255))
+    # """ Web log url.
+    # """
+
 
 
 
