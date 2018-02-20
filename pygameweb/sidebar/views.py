8778ef2c45db5086e780fd6e7efd903160d171ce
René Dudfield <renesd@gmail.com>
2018-02-20 05:44:51 +0100

    Recent releases only shows most recent release for a project.

--- pygameweb/sidebar/views.py
+++ pygameweb/sidebar/views.py
@@ -7,23 +7,19 @@ Side bar stuff includes::
 """
 
 from flask_sqlalchemy_session import current_session
-from pygameweb.project.models import top_tags, Project, Release
+from pygameweb.project.models import top_tags, recent_releases
 from pygameweb.user.models import User
 
 
 def sidebar():
     """ called by template in base.html sidebar block.
     """
-    top_tag_counts = top_tags(current_session)
-
-    recent_releases = (current_session.query(User, Project, Release)
-                       .filter(Release.project_id == Project.id)
-                       .filter(User.id == Project.users_id)
-                       .filter(User.disabled == 0)
-                       .order_by(Release.datetimeon.desc())
+    return dict(
+      top_tag_counts=top_tags(current_session),
+      recent_releases=(recent_releases(current_session)
                        .limit(10)
                        .all())
-    return dict(top_tag_counts=top_tag_counts, recent_releases=recent_releases)
+    )
 
 
 def add_sidebar(app):
