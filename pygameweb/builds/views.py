e4e64957a3b457cd04c8902f6b1f6dc013ef4b93
René Dudfield <renesd@gmail.com>
2018-02-21 16:03:11 +0100

    pygameweb.builds.views now listens for github master commits

--- pygameweb/builds/views.py
+++ pygameweb/builds/views.py
@@ -1,17 +1,48 @@
-"""bitbucket.org related things.
+"""Building pygame, and doing stuff when things are merged.
+
+github.com related webhook integration.
 """
-from subprocess import run
 import os
+import json
+from subprocess import run
+import hmac
+import hashlib
+from flask import request, abort
 
-bitbucket_url = os.environ.get('APP_BITBUCKET_URL', '/bitbucket')
 
+github_url = os.environ.get('APP_GITHUB_WEBHOOK_URL', '/github')
+github_secret = bytes(os.environ.get('APP_GITHUB_WEBHOOK_SECRET', ''), 'utf8')
 
+def verify_github(secret, body, x_hub_signature):
+    """ https://developer.github.com/webhooks/securing/
+    """
+    signature = 'sha1=' + hmac.new(secret, body, hashlib.sha1).hexdigest()
+    return signature == x_hub_signature
 
 def add_builds(app):
     """ to the app.
     """
-    @app.route(bitbucket_url, methods=['POST', 'GET'])
-    def bitbucket(**kwargs):
-        run(['pygameweb_update_docs'])
-        run(['pygameweb_launchpad'])
+
+    @app.route(github_url, methods=['POST', 'GET'])
+    def github_webhook(**kwargs):
+        """Called by github when something happens.
+
+        When something goes on master branch, we:
+          - build the documentation,
+          - update the launchpad badge.
+
+        https://developer.github.com/webhooks/
+        """
+        if not verify_github(github_secret,
+                             request.get_data(),
+                             request.headers.get('X-Hub-Signature', '')):
+            abort(404)
+
+        payload = request.form.get('payload', None)
+        if payload:
+            data = json.loads(payload)
+            if hasattr(data, 'get'):
+                if data.get('ref', None) == 'refs/heads/master':
+                    run(['pygameweb_update_docs'])
+                    run(['pygameweb_launchpad'])
         return 'sweet'
