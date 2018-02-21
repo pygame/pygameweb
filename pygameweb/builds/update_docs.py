57fee89ded2920d1fa896ea02ea86469a4607c74
René Dudfield <renesd@gmail.com>
2018-02-21 16:06:58 +0100

    Fix git pull command.

--- pygameweb/builds/update_docs.py
+++ pygameweb/builds/update_docs.py
@@ -26,7 +26,7 @@ def update_docs():
     assert update_file.exists()
 
     run(['touch', str(update_file)], cwd=cwd)
-    run(['git pull', 'pull'], cwd=cwd)
+    run(['git', 'pull'], cwd=cwd)
     run([str(venv_python.absolute()), 'makeref.py'], cwd=cwd)
     run(['rsync', '-va', '--delete', 'docs/',
          str(docs_path.absolute())], cwd=cwd)
