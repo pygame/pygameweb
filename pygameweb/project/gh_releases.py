5454fc8f0bc8be54a2ae7c05dc6c7b9108e0d51f
René Dudfield <renesd@gmail.com>
2019-01-13 18:28:05 +0100

    Some tests for gh release syncing.

--- pygameweb/project/gh_releases.py
+++ pygameweb/project/gh_releases.py
@@ -1,23 +1,67 @@
 """ For syncing github releases to pygame releases.
 """
-import feedparser
 import urllib.parse
+import feedparser
 
-def sync_project(project):
-    if not project.github_repo:
-        return
-    gh_releases = get_gh_releases(project)
-    # get ones not as pygame project releases.
-    gh_versions = {r.title for r in gh_releases}
-    pg_versions = {r.version for r project.releases}
-    versions_to_add = pg_versions - gh_versions
 
-    gh_releases_to_add = [r for r in gh_releases if r.title in versions_to_add]
+# def sync_project(session, project):
+#     if not project.github_repo:
+#         return
+#     gh_releases = get_gh_releases(project)
+#     releases = project.releases
+#     gh_add, gh_update, pg_delete = releases_to_sync(gh_releases, releases)
 
-    # TODO: pg releases with from_external == 'github' can be updated.
-    # gh_releases_to_update =
-    raise NotImplmentedError()
+#     #TODO: deal with gh_add, gh_update, pg_delete
+#     for gh_release in gh_add:
+#         session.add(release_from_gh(gh_release))
 
+#     for gh_release in gh_update:
+#         release = session. get Release     gh_release   ...
+#         release.version = gh_release.title
+#         release. changes = gh_release. xx
+#         session.add(release)
+
+#     for pg_release in pg_delete:
+#         release = session. get Release   pg_release   ...
+#         release.delete()
+#         session.add(release)
+
+
+
+# def release_from_gh(gh_release):
+#     """ make a Release from a gh release.
+#     """
+
+#     ...
+
+
+
+def releases_to_sync(gh_releases, releases):
+    add, update, delete = versions_to_sync(gh_releases, releases)
+
+    gh_add = [r for r in gh_releases if r.title in add]
+    gh_update = [r for r in gh_releases if r.title in update]
+    pg_delete = [r for r in releases if r.version in delete]
+    return gh_add, gh_update, pg_delete
+
+def versions_to_sync(gh_releases, releases):
+
+    # Because many projects might have existing ones on pygame,
+    #  but not have them on github, we don't delete ones unless
+    #  they came originally from github.
+    return what_versions_sync(
+        {r.version for r in releases},
+        {r.title for r in gh_releases},
+        {r.version for r in releases if r.from_external == 'github'}
+    )
+
+def what_versions_sync(pg_versions, gh_versions, pg_versions_gh):
+    """ versions to add, update, delete.
+    """
+    to_add = gh_versions - pg_versions
+    to_update = pg_versions_gh & gh_versions
+    to_delete = pg_versions_gh - gh_versions
+    return to_add, to_update, to_delete
 
 def get_gh_releases(project):
     """ for a project.
@@ -33,4 +77,3 @@ def get_gh_releases(project):
     if not data['title'].starts_with('Release notes from'):
         raise ValueError('does not appear to be a github release feed.')
     return data.entries
-
--- tests/functional/pygameweb/project/test_gh_releases.py
+++ tests/functional/pygameweb/project/test_gh_releases.py
@@ -1,1 +1,60 @@
-test_gh_releases.py
+
+import pytest
+
+
+class Release:
+    def __init__(self, version, description, from_external):
+        self.version = version
+        self.description = description
+        self.from_external = from_external
+
+class GRelease:
+    def __init__(self, title, content):
+        self.title = title
+        self.content = [{'value': content}]
+
+def make_gh(releases):
+    return [GRelease(r[0], r[1]) for r in releases]
+
+def make_pg(releases):
+    return [Release(r[0], r[1], r[2]) for r in releases]
+
+
+def test_releases_to_sync():
+    """ adds the one that is missing.
+    """
+    from pygameweb.project.gh_releases import releases_to_sync
+    pg_releases = make_pg([('0.1', 'r1', None), ('0.2', 'r2', None)])
+    gh_releases = make_gh([('0.1', 'r1'), ('0.2', 'r2'), ('0.3', 'r3')])
+    gh_add, gh_update, pg_delete = releases_to_sync(gh_releases, pg_releases)
+
+    assert len(gh_add) == 1
+    assert not gh_update and not pg_delete
+    assert gh_add[0].title == '0.3'
+
+def test_releases_to_sync_delete():
+    """ deletes the github updated one that is there.
+    """
+    from pygameweb.project.gh_releases import releases_to_sync
+
+    pg_releases = make_pg([('0.1', 'r1', None), ('0.2', 'r2', 'github')])
+    gh_releases = make_gh([('0.1', 'r1')])
+    gh_add, gh_update, pg_delete = releases_to_sync(gh_releases, pg_releases)
+
+    assert not gh_add and not gh_update
+    assert len(pg_delete) == 1
+    assert pg_delete[0].version == '0.2'
+
+def test_releases_to_sync_update():
+    """ updates the github one that changed.
+    """
+    from pygameweb.project.gh_releases import releases_to_sync
+
+    pg_releases = make_pg([('0.1', 'r1', None), ('0.2', 'r2', 'github')])
+    gh_releases = make_gh([('0.1', 'r1'), ('0.2', 'r2 with updated content')])
+    gh_add, gh_update, pg_delete = releases_to_sync(gh_releases, pg_releases)
+
+    assert len(gh_update) == 1
+    assert not gh_add and not pg_delete
+    assert gh_update[0].title == '0.2'
+    assert gh_update[0].content[0]['value'] == 'r2 with updated content'
