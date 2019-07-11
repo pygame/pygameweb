eb0e5321c667d0713a5b83299a5d06656b017a83
René Dudfield <renesd@gmail.com>
2019-07-11 12:22:24 +0200

    Fix test_app.py

--- tests/functional/pygameweb/test_app.py
+++ tests/functional/pygameweb/test_app.py
@@ -1,4 +1,4 @@
-
+import pytest
 
 def test_create_app(app):
     """ can we create an app?
@@ -6,12 +6,6 @@ def test_create_app(app):
     assert app
     assert app.engine
 
-def test_custom_error_page_404(client):
-    """ is shown when a route does not exist.
-    """
-    resp = wiki_client.get('/thisshouldnotexist')
-    assert resp.status_code == 404
-    assert b'<a href="/">Home</a>' in resp.data
 
 @pytest.fixture
 def error_client(app, client):
@@ -22,10 +16,15 @@ def error_client(app, client):
         raise Exception('oops')
     return client
 
-def test_custom_error_page_500(client):
+def test_custom_error_page_500(error_client):
     """ is shown on a 500 error"""
-    resp = wiki_client.get('/this500s')
+    resp = error_client.get('/this500s')
     assert resp.status_code == 500
     assert b'<a href="/">Home</a>' in resp.text
-    #from pyquery import PyQuery as pq
-    #assert pq(resp.text).find('a:contains(Home)').length
+
+def test_custom_error_page_404(error_client):
+    """ is shown when a route does not exist.
+    """
+    resp = error_client.get('/thisshouldnotexist')
+    assert resp.status_code == 404
+    assert b'<a href="/">Home</a>' in resp.data
