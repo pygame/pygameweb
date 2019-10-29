9e9bad4bfb4f3850d90100b6bc53781c146a4676
René Dudfield <renesd@gmail.com>
2019-10-29 18:49:51 +0100

    If there are news_alert show it on the docs.

--- pygameweb/comment/static/jquery.plugin.docscomments.js
+++ pygameweb/comment/static/jquery.plugin.docscomments.js
@@ -1,121 +1,121 @@
 $(document).ready(function () {
 
 
-	/*
-	- fix comment addition for module level comment.
-	- format dates in a more human readable way.
-	X - show user title for each post.
-	X - order comments by newest first.
-	X - prevent comments from being added multiple times.
-	X - add comment button to reference full url.
-	X - Add 'add comment' button in, even if there are comments.
-	*/
+    /*
+    - fix comment addition for module level comment.
+    - format dates in a more human readable way.
+    X - show user title for each post.
+    X - order comments by newest first.
+    X - prevent comments from being added multiple times.
+    X - add comment button to reference full url.
+    X - Add 'add comment' button in, even if there are comments.
+    */
 
-	var _comments = {};
+    var _comments = {};
 
-	$.getJSON("https://www.pygame.org/docs/ref/docscomments.json", function (data) {
-		//console.log(data);
+    $.getJSON("https://www.pygame.org/docs/ref/docscomments.json", function (data) {
+        //console.log(data);
 
-		// newest comment first.
-		var sortComments = function (a,b) {
-		    var x = a.datetimeon;
-		    var y = b.datetimeon;
-		    return ((x < y) ? 1 : ((x > y) ? -1 : 0));
-		}
+        // newest comment first.
+        var sortComments = function (a,b) {
+            var x = a.datetimeon;
+            var y = b.datetimeon;
+            return ((x < y) ? 1 : ((x > y) ? -1 : 0));
+        }
 
-		for(var i=0; i < data.length; i+=1) {
-			var link = data[i]['link']
-			if(typeof _comments[link] === "undefined") {
-				_comments[link] = [];
-			}
-			_comments[link].push(data[i]);
-			_comments[link].sort(sortComments);
-		}
-
-
-		var getComments = function (link) {
-			return _comments[link];
-		};
+        for(var i=0; i < data.length; i+=1) {
+            var link = data[i]['link']
+            if(typeof _comments[link] === "undefined") {
+                _comments[link] = [];
+            }
+            _comments[link].push(data[i]);
+            _comments[link].sort(sortComments);
+        }
 
 
-		// var addCommentHtml = [
-		// 	'<form action="http://www.pygame.org/docs/ref/comment_new.php" class="addcomment"><input type="hidden" value="',
-		// 	'" name="link"><input type="submit" value="Add a Comment"></form>'
-		// ];
-		var showCommentHtml = [
-			'<a href="#comment_',
-			'" title="show comments" class="commentButton">Comments ',
-			'</a><article id="comment_',
-			'" class="hidden commentsArticle"></article>'
-		];
-		var commentPartHtml = [
-			'<section class="commentPart"><header class="commentHeading">',
-			'</header>',
-			'<pre class="commentContent">',
-			'</pre></section>'
-		]
+        var getComments = function (link) {
+            return _comments[link];
+        };
 
-		$('dt.title').each(function (idx, el) {
-			var link = $(el).attr('id');
-			if (typeof link === "undefined") {
-				return;
-			}
 
-			// Add "search internet for source code" buttons.
-			//var searchButton = $(searchButtonHtml[0] + link + searchButtonHtml[1] + link + searchButtonHtml[2]);
-			//$(el).next().append(searchButton);
+        // var addCommentHtml = [
+        //  '<form action="http://www.pygame.org/docs/ref/comment_new.php" class="addcomment"><input type="hidden" value="',
+        //  '" name="link"><input type="submit" value="Add a Comment"></form>'
+        // ];
+        var showCommentHtml = [
+            '<a href="#comment_',
+            '" title="show comments" class="commentButton">Comments ',
+            '</a><article id="comment_',
+            '" class="hidden commentsArticle"></article>'
+        ];
+        var commentPartHtml = [
+            '<section class="commentPart"><header class="commentHeading">',
+            '</header>',
+            '<pre class="commentContent">',
+            '</pre></section>'
+        ]
 
-			// Add show comments buttons.
-			var comments = getComments(link);
+        $('dt.title').each(function (idx, el) {
+            var link = $(el).attr('id');
+            if (typeof link === "undefined") {
+                return;
+            }
 
-			// var addCommentButton = $(addCommentHtml[0] + link + addCommentHtml[1]);
-			// $(el).next().append(addCommentButton);
+            // Add "search internet for source code" buttons.
+            //var searchButton = $(searchButtonHtml[0] + link + searchButtonHtml[1] + link + searchButtonHtml[2]);
+            //$(el).next().append(searchButton);
 
-			if(typeof comments === "undefined") {
-			} else {
-				// show comment button.
-				//console.log(link)
-				var $showCommentButton = $([showCommentHtml[0],
-							  link.replace(/\./g, "_"),
-							showCommentHtml[1],
-							comments.length,
-							showCommentHtml[2],
-							  link.replace(/\./g, "_"),
-							showCommentHtml[3]
-							  ].join(""));
-				$(el).next().append($showCommentButton);
+            // Add show comments buttons.
+            var comments = getComments(link);
 
-				$showCommentButton.click(function () {
-					//console.log('asdf')
-					var $commentSection = $("#comment_" + link.replace(/\./g, "_"));
-					if (!$commentSection.hasClass('hidden')) {
-						// we have already loaded the comments for this part.
-						return;
-					}
-					$commentSection.removeClass("hidden");
-					$.each(comments, function(idx) {
-						console.log(comments[idx])
-						// date + user
-						var userName = comments[idx]['user_title'];
-						if (userName == null) {
-							userName = 'Anonymous';
-						}
-						var commentHeading = comments[idx]['datetimeon'] + " - " + userName;
-						var commentContent= comments[idx]['content'];
+            // var addCommentButton = $(addCommentHtml[0] + link + addCommentHtml[1]);
+            // $(el).next().append(addCommentButton);
 
-						var $commentPart = $([commentPartHtml[0],
-							  commentHeading,
-							commentPartHtml[1],
-							commentPartHtml[2],
-							commentContent,
-							commentPartHtml[3]].join("\n"));
-						$commentSection.append($commentPart);
-					});
+            if(typeof comments === "undefined") {
+            } else {
+                // show comment button.
+                //console.log(link)
+                var $showCommentButton = $([showCommentHtml[0],
+                              link.replace(/\./g, "_"),
+                            showCommentHtml[1],
+                            comments.length,
+                            showCommentHtml[2],
+                              link.replace(/\./g, "_"),
+                            showCommentHtml[3]
+                              ].join(""));
+                $(el).next().append($showCommentButton);
 
-				})
-			}
-		});
-	});
+                $showCommentButton.click(function () {
+                    //console.log('asdf')
+                    var $commentSection = $("#comment_" + link.replace(/\./g, "_"));
+                    if (!$commentSection.hasClass('hidden')) {
+                        // we have already loaded the comments for this part.
+                        return;
+                    }
+                    $commentSection.removeClass("hidden");
+                    $.each(comments, function(idx) {
+                        console.log(comments[idx])
+                        // date + user
+                        var userName = comments[idx]['user_title'];
+                        if (userName == null) {
+                            userName = 'Anonymous';
+                        }
+                        var commentHeading = comments[idx]['datetimeon'] + " - " + userName;
+                        var commentContent= comments[idx]['content'];
+
+                        var $commentPart = $([commentPartHtml[0],
+                              commentHeading,
+                            commentPartHtml[1],
+                            commentPartHtml[2],
+                            commentContent,
+                            commentPartHtml[3]].join("\n"));
+                        $commentSection.append($commentPart);
+                    });
+
+                })
+            }
+        });
+    });
 
 
     var searchButtonHtml = [
@@ -133,4 +133,39 @@ $(document).ready(function () {
         var searchButton = $(searchButtonHtml[0] + link + searchButtonHtml[1] + link + searchButtonHtml[2]);
         $(el).next().append(searchButton);
     });
+
+
+    var $newsAlert = $('<span></span>');
+    $newsAlert.load('/news-alert-view/1', function() {
+        if($newsAlert.children().length > 1) {
+            $("    <style>" +
+              "    #docs-header {" +
+              "        position: fixed;" +
+              "        top: 0;" +
+              "        left: 0;" +
+              "        right: 0;" +
+              "        height: auto;" +
+              "        text-align: center;" +
+              "        color: white;" +
+              "        background-color: black;" +
+              "        font-size: larger;" +
+              "        line-height: 1.5;" +
+              "        padding-top: 13px;" +
+              "        padding-bottom: 13px;" +
+              "        z-index: 999;" +
+              "    }" +
+              "    #docs-header > a {" +
+              "        color: white;" +
+              "        text-decoration: none;" +
+              "        border-bottom: 1px dotted #999;" +
+              "    }" +
+              "    .document .header {" +
+              "      padding-top: 55px;" +
+              "    }" +
+              "    </style>").appendTo("head");
+
+            $newsAlert.appendTo('body');
+        }
+    });
+
 });
